c6bb2265fa455a6c2918b34244a8f48d9e7eae19
jian玄冰 <jianxuanhuo1@126.com>
2019-05-25 23:14:08 +0800

    修改 枚举描述文档过滤器

--- src/Bing.Extensions.Swashbuckle/Filters/Documents/AddEnumDescriptionsDocumentFilter.cs
+++ src/Bing.Extensions.Swashbuckle/Filters/Documents/AddEnumDescriptionsDocumentFilter.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -11,9 +10,12 @@ namespace Bing.Extensions.Swashbuckle.Filters.Documents
     /// </summary>
     public class AddEnumDescriptionsDocumentFilter : IDocumentFilter
     {
+        /// <summary>
+        /// 重写操作处理
+        /// </summary>
         public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
         {
-            // add enum descriptions to result models
+            // 添加枚举描述到结果模型
             foreach (var schemaDictionaryItem in swaggerDoc.Definitions)
             {
                 var schema = schemaDictionaryItem.Value;
@@ -29,9 +31,12 @@ namespace Bing.Extensions.Swashbuckle.Filters.Documents
                 }
             }
 
-            if (swaggerDoc.Paths.Count <= 0) return;
+            if (swaggerDoc.Paths.Count <= 0)
+            {
+                return;
+            }
 
-            // add enum descriptions to input parameters
+            // 添加枚举描述到输入参数
             foreach (var pathItem in swaggerDoc.Paths.Values)
             {
                 DescribeEnumParameters(pathItem.Parameters);
@@ -46,8 +51,7 @@ namespace Bing.Extensions.Swashbuckle.Filters.Documents
         /// <summary>
         /// 获取字符串映射
         /// </summary>
-        /// <param name="enums"></param>
-        /// <returns></returns>
+        /// <param name="enums">枚举值</param>
         private string[] GetStringMapping(IList<object> enums)
         {
             var enumDescriptions = new List<string>();
@@ -61,9 +65,16 @@ namespace Bing.Extensions.Swashbuckle.Filters.Documents
             return enumDescriptions.ToArray();
         }
 
+        /// <summary>
+        /// 描述枚举参数
+        /// </summary>
+        /// <param name="parameters">参数</param>
         private static void DescribeEnumParameters(IList<IParameter> parameters)
         {
-            if (parameters == null) return;
+            if (parameters == null)
+            {
+                return;
+            }
 
             foreach (var param in parameters)
             {
@@ -71,7 +82,7 @@ namespace Bing.Extensions.Swashbuckle.Filters.Documents
                 {
                     if (nonBodyParameter.Enum != null && nonBodyParameter.Enum.Count > 0)
                     {
-                        param.Description += GetEnumDescription(nonBodyParameter.Enum);
+                        param.Description += GetEnumDescription(nonBodyParameter.Enum, false);
                     }
                 }
             }
@@ -81,19 +92,23 @@ namespace Bing.Extensions.Swashbuckle.Filters.Documents
         /// 获取枚举描述字符串
         /// </summary>
         /// <param name="enums">枚举值</param>
-        /// <returns></returns>
-        private static string GetEnumDescription(IEnumerable<object> enums)
+        /// <param name="isWrap">是否换行</param>
+        private static string GetEnumDescription(IEnumerable<object> enums,bool isWrap = true)
         {
             var enumDescriptions = new List<string>();
             Type type = null;
             foreach (var enumOption in enums)
             {
-                if (type == null) type = enumOption.GetType();
+                if (type == null)
+                {
+                    type = enumOption.GetType();
+                }
                 enumDescriptions.Add(
                     $"{Convert.ChangeType(enumOption, type.GetEnumUnderlyingType())} = {Internal.Enum.GetDescription(type, enumOption)}");
             }
 
-            return $"{Environment.NewLine}{string.Join(Environment.NewLine, enumDescriptions)}";
+            var separator = isWrap ? Environment.NewLine : "; ";
+            return $"{Environment.NewLine}{string.Join(separator, enumDescriptions)}";
         }
     }
 }
